8e403c5f042194ea9bbff4aca6e780b56e7b6340
87stal <87stal@gmail.com>
2021-03-30 16:30:24 +0300

    Adding gravatar, Multer, controller and route avatars

--- model/users.js
+++ model/users.js
@@ -17,9 +17,13 @@ const updateToken = async (id, token) => {
   return await User.updateOne({ _id: id }, { token })
 }
 
+const updateAvatar = async (id, avatar) => {
+  return await User.updateOne({ _id: id }, { avatar })
+}
 module.exports = {
   findByEmail,
   findById,
   create,
-  updateToken
+  updateToken,
+  updateAvatar
 }
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -3,10 +3,16 @@ const router = express.Router()
 const validate = require('./validation')
 const userController = require('../../../controllers/users')
 const guard = require('../../../helpers/guard')
+const upload = require('../../../helpers/upload')
+const { createAccountLimiter } = require('../../../helpers/rate-limit-reg')
 
-router.post('/auth/register', validate.schemaRegisterUser, userController.reg)
+router.post('/auth/register', createAccountLimiter, validate.schemaRegisterUser, userController.reg)
 router.post('/auth/login', validate.schemaLoginUser, userController.login)
 router.post('/auth/logout', guard, userController.logout)
 router.get('/users/current', guard, userController.currentUser)
-
+router.patch(
+  '/avatars',
+  [guard, upload.single('avatar'), validate.validateUploadAvatar],
+  userController.avatars
+)
 module.exports = router
